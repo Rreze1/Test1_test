5b6bc9f70fb0766a516050abff47ada254e9aa79
Rreze <rreze@cybertekschool.com>
2023-07-11 19:51:45 +0200

    committing

--- src/Test1.java
+++ src/Test1.java
@@ -2,5 +2,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("testtest");
         System.out.println("I want to create a conflict");
+        System.out.println("testtestttststts");
     }
 }
