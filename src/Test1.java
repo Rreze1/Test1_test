2024be70ae9db72927fe1d9eb77de1df8803bd1e
Rreze <rreze@cybertekschool.com>
2023-07-11 19:54:22 +0200

    test

--- src/Test1.java
+++ src/Test1.java
@@ -2,5 +2,7 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("testtest");
         System.out.println("I want to create a conflict");
+        System.out.println("dfljsdlkf");
+        System.out.println("dfkljdslkjfds");
     }
 }
